38519ab5aa2f375d99126673497b634c113fbffa
bhumika-1234 <140685553+bhumika-1234@users.noreply.github.com>
2024-05-04 07:48:42 +0530

    Add files via upload

---
+++ Dicee+Challenge+-+Starting+Files (2)/index.js
@@ -0,0 +1,5 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+document.querySelector('.img1').setAttribute("src","./images/dice"+randomnumber1+".png");
+
+var randomnumber2=Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src","./images.dice"+randomnumber2+".png");
